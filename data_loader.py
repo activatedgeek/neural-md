65cbfd0e4217556f058863a42962469c71dde4bf
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2018-04-19 16:36:14 -0400

    Batch chains into length 64, update data loader

--- data_loader.py
+++ data_loader.py
@@ -5,6 +5,9 @@ from torch.utils.data import Dataset
 
 
 class PDBChainsDataLoader(Dataset):
+    NPY_EXT = '.npy'
+    FASTA_EXT = '.faa'
+
     def __init__(self, chains_path):
         """
         Takes a folder which contains chain ".npy" files and ".fst" files
@@ -12,29 +15,33 @@ class PDBChainsDataLoader(Dataset):
         """
         self.chains_path = chains_path
 
-        # Get a list of all chains
-        self.chains = list(
-            map(
-                lambda f: os.path.splitext(f)[0],
-                filter(
-                    lambda f: '.fst' in f,
-                    os.listdir(chains_path)
-                )
-            )
-        )
+        # Get a list of all chains from all FASTA files
+        self.chains = []
+        # self.seq_strings = []
+
+        for f_name in os.listdir(self.chains_path):
+            if self.FASTA_EXT in f_name:
+                faa_path = os.path.join(self.chains_path, f_name)
+                with open(faa_path, 'r') as fasta:
+                    for line in fasta:
+                        if line[0] == '>':
+                            chain_id = line[1:].split('|')[0]
+                            self.chains.append(chain_id)
+                        # else:
+                        #     self.seq_strings.append(line)
 
     def __len__(self):
         return len(self.chains)
 
     def __getitem__(self, item):
         """
-        @NOTE: Only reading chain file here to be memory efficient
+        @NOTE: Only reading numpy file here to be memory efficient
         """
-        chain = self.chains[item]
-        npy_path = os.path.join(self.chains_path, chain + '.npy')
+        chain_id = self.chains[item]
+        npy_path = os.path.join(self.chains_path, chain_id + self.NPY_EXT)
         chain_data = np.load(npy_path)
-        chain_tensor = torch.from_numpy(chain_data)
-        fasta_path = os.path.join(self.chains_path, chain + '.fst')
-        with open(fasta_path, 'r') as f:
-            seq_string = f.readlines()[1].rstrip('\n')
-        return chain_tensor, seq_string
+        chain_tensor = torch.from_numpy(chain_data).float()
+
+        # seq_string = self.seq_strings[item]
+
+        return chain_tensor
--- pdb2data.py
+++ pdb2data.py
@@ -67,60 +67,93 @@ def aa_to_vector(pose, r_i):
     return letter, feature_vector
 
 
+def pre_compute_chains(pose, batch_size=None):
+    """
+    Precompute all the set of chains. Batch if needed. If a batched chain is
+    of size smaller than batch, it is dropped. The output is a Python dictionary
+    mapping from chain_id (string) to residue number limits (a tuple(int,int)),
+    e.g. {'A1': (65,128)} represents a chain fragment of size 64 containing residues
+    from atom number 65 to 128. This works because all residues of a chain occur in
+    sequence in the PDB.
+    :param pose:
+    :param batch_size:
+    :return: a dictionary mapping the chain_id to tuple of range of atom numbers
+    """
+    chains = {}
+    for i in range(1, pose.total_residue() + 1):
+        chain_id = pose.pdb_info().chain(i)
+        if chain_id not in chains:
+            chains[chain_id] = (i, i)
+        chains[chain_id] = (chains[chain_id][0], i)
+
+    if batch_size is not None:
+        batched_chains = {}
+        for k, (l, r) in chains.items():
+            batch_count = (r - l + 1) // batch_size
+            for b_id in range(batch_count):
+                batched_chains[k + str(b_id)] = (l + b_id * batch_size, l + (b_id + 1) * batch_size - 1)
+
+        return batched_chains
+
+    return chains
+
+
 def pdb_to_chains(pdb_path):
     pdb_file = os.path.basename(pdb_path)
     pdb_id, _ = os.path.splitext(pdb_file)
 
     # Generate chains from PDB
     pose = pyrosetta.pose_from_pdb(pdb_path)
-    chains = {}
-    seqs = {}
-    for i in range(1, pose.total_residue() + 1):
-        try:
-            seq_id, residue_vec = aa_to_vector(pose, i)
-            chain_id = pose.pdb_info().chain(i)
-            if chain_id not in chains:
-                chains[chain_id] = []
-            if chain_id not in seqs:
-                seqs[chain_id] = ''
-            seqs[chain_id] += seq_id
-            chains[chain_id].append(residue_vec)
-        except AssertionError as e:
-            print('Could not vectorize residue {}: {}'.format(i, str(e)))
+    chains = pre_compute_chains(pose, batch_size=64)
 
-    return seqs, chains
+    for chain_id, (residue_l, residue_r) in chains.items():
+        seq_string = ''
+        chain_list = []
+        for r_id in range(residue_l, residue_r + 1):
+            try:
+                r_letter, r_vec = aa_to_vector(pose, r_id)
+                seq_string += r_letter
+                chain_list.append(r_vec)
+            except AssertionError as e:
+                print('Could not vectorize residue {}: {}, skipping chain {}'.format(r_id, str(e), chain_id))
+                continue
+
+        yield chain_id, seq_string, chain_list
 
 
-def chains_to_file(pdb_id, seqs, chains, out_folder=CHAINS_DIR):
+def make_fasta(pdb_id, chain_id, seq):
+    return '>{}_{}|PDBID|CHAIN|SEQUENCE\n{}\n'.format(pdb_id, chain_id, seq)
+
+
+def process_pdb_list(files):
     try:
-        os.makedirs(out_folder)
+        os.makedirs(CHAINS_DIR)
     except OSError as e:
         if e.errno != os.errno.EEXIST:
             raise
 
-    for k, v in seqs.items():
-        fasta_string = '>{}:{}|PDBID|CHAIN|SEQUENCE\n{}\n'.format(pdb_id, k, v)
-        with open('{}/{}_{}.fst'.format(out_folder, pdb_id, k), 'w') as f:
-            f.write(fasta_string)
-
-    for chain_id, chain in chains.items():
-        out_path = '{}/{}_{}.npy'.format(out_folder, pdb_id, chain_id)
-        np.save(out_path, np.array(chain))
-
-
-def process_pdb_list(files):
+    processed_count = 0
     for f in files:
         pdb_id = os.path.splitext(f)[0]
         pdb_path = os.path.join(PDB_DIR, f)
-        if os.path.isdir(os.path.join(CHAINS_DIR, pdb_id)):
+
+        faa_path = '{}/{}.faa'.format(CHAINS_DIR, pdb_id)
+        if os.path.isfile(faa_path):
             print('{} already processed'.format(pdb_path))
-        else:
-            try:
-                seqs, chains = pdb_to_chains(pdb_path)
-                chains_to_file(pdb_id, seqs, chains)
-                print('Processed {}'.format(pdb_path))
-            except RuntimeError as e:
-                print('Unable to process {}: {}'.format(pdb_id, str(e)))
+            continue
+
+        with open(faa_path, 'w') as f:
+            for chain_id, seq_string, chain_list in pdb_to_chains(pdb_path):
+                fasta_string = make_fasta(pdb_id, chain_id, seq_string)
+                f.write(fasta_string)
+
+                out_path = '{}/{}_{}.npy'.format(CHAINS_DIR, pdb_id, chain_id)
+                np.save(out_path, np.array(chain_list).T) # 109 x 64 numpy array
+
+        print('Processed {}'.format(pdb_path))
+        processed_count += 1
+
+    print('{}/{} new PDBs processed'.format(processed_count, len(files)))
 
 
 class PDB2DataThread(threading.Thread):
